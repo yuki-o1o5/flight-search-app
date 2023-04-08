9c1a0dc74d20e8ab54fce6c75077d801d0996927
yuki-o1o5 <jkksst1712@gmail.com>
2023-04-07 18:25:29 -0700

    feat/add detailpage

---
+++ src/views/components/AccordionCard.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+type AccordionCardProps = {
+  information: {
+    id: number;
+    itineraries: Array<object>;
+    numberOfBookableSeats: number;
+    oneWay: boolean;
+    price: {
+      base: string;
+      currency: string;
+      grandTotal: string;
+    };
+    travelerPricings: Array<object>;
+  };
+};
+
+export default function AccordionCard({ information }: AccordionCardProps) {
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+            {information.numberOfBookableSeats}
+          </Typography>
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+            {information.price.grandTotal} {information.price.currency}
+          </Typography>
+          <Typography sx={{ width: "33%", flexShrink: 0 }}>
+            I am an accordion
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+            Aliquam eget maximus est, id dignissim quam.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
--- src/views/pages/Detail.tsx
+++ src/views/pages/Detail.tsx
@@ -1,5 +1,101 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { apiAuthorize } from "../../constants/apiAuthorize";
+import AccordionCard from "../components/AccordionCard";
 
 export default function Detail() {
-  return <div>Detail</div>;
+  // ----------------------------------------------------------------
+  // useState
+  // ----------------------------------------------------------------
+
+  const [flightDatas, setFlightDatas] = React.useState<Flight[]>([]);
+
+  // ----------------------------------------------------------------
+  // useEffect
+  // ----------------------------------------------------------------
+  useEffect(() => {
+    (async () => {
+      await fetchFlightData(
+        originalLocationCode,
+        destinationLocationCode,
+        departureDate,
+        adultNumber,
+        childNumber,
+        travelClass
+      );
+    })();
+  }, []);
+
+  console.log(flightDatas);
+
+  // ----------------------------------------------------------------
+  // Type
+  // ----------------------------------------------------------------
+
+  type Flight = {
+    id: number;
+    itineraries: Array<object>;
+    numberOfBookableSeats: number;
+    oneWay: boolean;
+    price: {
+      base: string;
+      currency: string;
+      grandTotal: string;
+    };
+    travelerPricings: Array<object>;
+  };
+
+  // ----------------------------------------------------------------
+  // function
+  // ----------------------------------------------------------------
+  const useQueryParams = () => {
+    const location = useLocation();
+    return new URLSearchParams(location.search);
+  };
+
+  const params = useQueryParams();
+
+  const originalLocationCode = params.get("originalLocationCode");
+  const destinationLocationCode = params.get("destinationLocationCode");
+  const departureDate = params.get("departureDate");
+  const adultNumber = params.get("adultNumber");
+  const childNumber = params.get("childNumber");
+  const travelClass = params.get("travelClass");
+
+  const fetchFlightData = async (
+    originalLocationCode: any,
+    destinationLocationCode: any,
+    departureDate: any,
+    adultNumber: any,
+    childNumber: any,
+    travelClass: any
+  ) => {
+    try {
+      const accessToken = await apiAuthorize();
+      const res = await axios.get(
+        `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originalLocationCode}&destinationLocationCode=${destinationLocationCode}&departureDate=${departureDate}&adults=${adultNumber}&children=${childNumber}&travelClass=${travelClass}&nonStop=false&currencyCode=USD&max=250`,
+
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      const result = res.data.data;
+      setFlightDatas(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="mt-2">
+        {flightDatas.map((flightData, index) => (
+          <AccordionCard key={index} information={flightData} />
+        ))}
+      </div>
+    </div>
+  );
 }
