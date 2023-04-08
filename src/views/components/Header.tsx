ade936f215e56317f2d69f56a7fc61903a034233
yuki-o1o5 <jkksst1712@gmail.com>
2023-04-07 18:24:32 -0700

    feat/add header

--- src/views/components/Header.tsx
+++ src/views/components/Header.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <a href="https://flowbite.com/" className="flex items-center">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-6 mr-3 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Flowbite
+          </span>
+        </a>
+      </nav>
+    </>
+  );
 }
