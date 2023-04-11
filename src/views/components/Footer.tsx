9377ae234938bbfe026d2db121620f69e535356c
yuki-o1o5 <jkksst1712@gmail.com>
2023-04-10 19:35:07 -0700

    style/change header footer

--- src/views/components/Footer.tsx
+++ src/views/components/Footer.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-rose-900 px-2 sm:px-4 py-10 dark:bg-gray-900  w-full z-20 top-0 left-0 text-white">
+      <div className="flex justify-end px-20 gap-20">
+        <div className="flex flex-col">
+          <p>Lorem ipsum</p>
+          <p>dolor sit amet</p>
+          <p>consectetur</p>
+          <p>repellat totam</p>
+          <p>qui veritatis</p>
+        </div>
+        <div className="flex flex-col">
+          <p> nisi delectus</p>
+          <p>ipsa numquam</p>
+          <p>cupiditate ipsum</p>
+          <p>cupiditate ipsum</p>
+          <p>ad maiores</p>
+        </div>
+      </div>
+      <div className="flex justify-center ">yuki</div>
+    </footer>
+  );
 }
