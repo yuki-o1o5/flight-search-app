f05d6401a4ecda5c31475a9194566d8b291aef24
yuki-o1o5 <jkksst1712@gmail.com>
2023-04-10 19:31:12 -0700

    docs/add img

---
+++ src/constants/images.tsx
@@ -0,0 +1,47 @@
+export const images = [
+  {
+    url: "../../images/roma.jpg",
+    cityName: "Roma",
+    iataCode: "FCO",
+  },
+  {
+    url: "../../images/paris.jpg",
+    cityName: "Paris",
+    iataCode: "CDG",
+  },
+  {
+    url: "../../images/london.jpg",
+    cityName: "London",
+    iataCode: "LHR",
+  },
+  {
+    url: "../../images/vancouver.jpg",
+    cityName: "Vancouver",
+    iataCode: "YVR",
+  },
+  {
+    url: "../../images/sanfrancisco.jpg",
+    cityName: "San Francisco",
+    iataCode: "SFO",
+  },
+  {
+    url: "../../images/hawaii.jpg",
+    cityName: "Hawaii",
+    iataCode: "HNL",
+  },
+  {
+    url: "../../images/mexico.jpg",
+    cityName: "Mexico",
+    iataCode: "MEX",
+  },
+  {
+    url: "../../images/bali.jpg",
+    cityName: "Bali",
+    iataCode: "DPS",
+  },
+  {
+    url: "../../images/tokyo.jpg",
+    cityName: "Tokyo",
+    iataCode: "NRT",
+  },
+];
